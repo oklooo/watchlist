f8f237b68432d6a55b7c399bae5783925f2562f9
czh <appleboarding@qq.com>
2019-02-03 09:14:07 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask,render_template 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy #导入扩展类
+import os
+import sys
+import click
 
-name = 'Grey Li'
-movies = [
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app) #初始化扩展，传入程序实例app,写入配置语句一般放到扩展类实例化语句之前
+
+class User(db.Model): #表名将会是user(自动生成，小写处理)
+    id = db.Column(db.Integer,primary_key=True) #主键
+    name = db.Column(db.String(20)) #名字
+
+class Movie(db.Model): #表名将会是movie
+    id = db.Column(db.Integer,primary_key=True) #主键
+    title = db.Column(db.String(60)) #电影标题
+    year = db.Column(db.String(4))  #电影年份
+
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+
+    db.create_all()
+    #全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
     {'title':'My Neighbor Totoro', 'year':'1988'},
     {'title':'Dead Poets Society', 'year':'1989'},
     {'title':'A Perfect World', 'year':'1993'},
@@ -13,8 +38,20 @@ movies = [
     {'title':'Devils on the Doorstep','year':'1999'},
     {'title':'WALL-E','year':'2008'},
     {'title':'The Pork of Music','year':'2012'},
-]
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name,movies=movies)
+    user = User.query.first() # 读取用户记录
+    movies = Movie.query.all() # 读取所有电影记录
+    return render_template('index.html', user=user,movies=movies)
