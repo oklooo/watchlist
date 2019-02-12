d3faa278cb869b2c33a9dd3673dc863dea4353b5
czh <appleboarding@qq.com>
2019-02-12 15:40:04 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -9,10 +9,10 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 #更新这里的路径，把app.root_path添加到os.path.dirname()中
 #以便把文件定位到项目根目录
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.dirname(app.root_path),'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.dirname(app.root_path),os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False 
 
 db = SQLAlchemy(app)
